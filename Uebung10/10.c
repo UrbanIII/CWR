53726a45180f8582934be8d40feddad6e7423f5a
GOESTERN-0465081 <m.marienhagen@c024.cip.physik.lokal>
2018-06-19 16:43:55 +0200

    Übung 10 - Random numbers

---
+++ Uebung10/10.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<math.h>
+#include <time.h>
+
+unsigned int * Linear_congruential_generator(unsigned long a, unsigned long b, unsigned long m, unsigned int seed, unsigned int n){ //Calculation is carried out with long unsigned to prevent overflow
+	unsigned int *array = malloc(sizeof(int)*n);
+	array[0] = seed;
+	for(int i = 1; i<n; i++){
+		array[i] = (unsigned int)((a*array[i-1]+b)%m);
+	}
+	return array;
+}
+
+unsigned int * R250(unsigned int *seed, int n){
+	unsigned int *array = malloc(sizeof(int)*(n+250));
+	for(int i = 0; i<250; i++){
+		array[i] = seed[i];
+	}
+	for(int i = 0; i<n; i++){
+		array[i+250] = array[i]^array[i+250-147];
+	}
+	return array;
+}
+
+int main(){
+	
+	unsigned long a = 22695477, b = 1, m = pow(2,32), seed = time(NULL); // For systems where ints have 4 bytes
+	
+	// Testing Linear_congruential_generator
+	FILE *file = fopen("lin_test_hist","w");
+	int n = 1000000;
+	unsigned int *lin_test = Linear_congruential_generator(a,b,m,seed,n); // Patterns for example for a = 343, b = 3348
+	for(int i = 0; i<n; i++){
+		fprintf(file,"%lf\n",(double)lin_test[i]/m);
+	}
+	fclose(file);
+	file = fopen("lin_test_2d","w");
+	for(int i=0; i<n; i+=2){
+		if(i+1<n){
+			fprintf(file,"%lf\t%lf\n",(double)lin_test[i]/m,(double)lin_test[i+1]/m);
+		}
+	}
+	fclose(file);
+	file = fopen("lin_test_3d","w");
+	for(int i=0; i<n; i+=3){
+		if(i+2<n){
+			fprintf(file,"%lf\t%lf\t%lf\n",(double)lin_test[i]/m,(double)lin_test[i+1]/m,(double)lin_test[i+2]/m);
+		}
+	}
+	fclose(file);  
+	free(lin_test);
+	
+	//Combine the two generators
+	unsigned int *start = Linear_congruential_generator(a,b,m,seed,250);
+	unsigned int *r_test = R250(start, n);
+	file = fopen("r_test_hist","w");
+	for(int i = 0; i<n; i++){
+		fprintf(file,"%lf\n",(double)r_test[i]/m);
+	}
+	fclose(file);
+	file = fopen("r_test_2d","w");
+	for(int i=0; i<n; i+=2){
+		if(i+1<n){
+			fprintf(file,"%lf\t%lf\n",(double)r_test[i]/m,(double)r_test[i+1]/m);
+		}
+	}
+	fclose(file);
+	file = fopen("r_test_3d","w");
+	for(int i=0; i<n; i+=3){
+		if(i+2<n){
+			fprintf(file,"%lf\t%lf\t%lf\n",(double)r_test[i]/m,(double)r_test[i+1]/m,(double)r_test[i+2]/m);
+		}
+	}  
+	free(r_test);
+	fclose(file);
+	
+}
+
---
+++ Uebung10/lin_test.gp
@@ -0,0 +1,34 @@
+set term x11 1
+
+# Histogram stolen from http://psy.swansea.ac.uk/staff/carter/gnuplot/gnuplot_frequency.htm
+
+clear
+reset
+set key off
+set border 3
+
+# Add a vertical dotted line at x=0 to show centre (mean) of distribution.
+set yzeroaxis
+
+# Each bar is half the (visual) width of its x-range.
+set boxwidth 0.001 absolute
+set style fill solid 1.0 noborder
+
+bin_width = 0.001;
+
+bin_number(x) = floor(x/bin_width)
+
+rounded(x) = bin_width * ( bin_number(x) + 0.5 )
+
+plot 'lin_test_hist' using (rounded($1)):(1) smooth frequency with boxes
+
+
+
+# 2D
+set term x11 2
+plot 'lin_test_2d' with points ps 1 pt 0
+
+
+# 3D
+set term x11 3
+splot 'lin_test_3d' with points pt 26 ps 0.5
